0130cd847ddd925041f18a6fda7d0e4a71bea98b
Peter Johnston <pemjohns@gmail.com>
2018-11-03 18:05:19 -0600

    Chapter 3.4 Evaluate Prefix and Infix expressions

--- evaluator/eval.go
+++ evaluator/eval.go
@@ -15,6 +15,13 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return booleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	default:
 		return object.NULL
 	}
@@ -26,6 +33,76 @@ func evalStatement(statements []ast.Statement) object.Object {
 		result = Eval(stmt)
 	}
 	return result
+}
+
+func evalPrefixExpression(op string, right object.Object) object.Object {
+	switch op {
+	case "!":
+		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusOperatorExpression(right)
+	default:
+		return object.NULL
+	}
+}
+
+func evalInfixExpression(op string, left, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(op, left, right)
+	case op == "==":
+		return booleanObject(left == right)
+	case op == "!=":
+		return booleanObject(left != right)
+	default:
+		return object.NULL
+	}
+}
+
+func evalIntegerInfixExpression(op string, left, right object.Object) object.Object {
+	lVal := left.(*object.Integer).Value
+	rVal := right.(*object.Integer).Value
+	switch op {
+	case "+":
+		return &object.Integer{Value: lVal + rVal}
+	case "-":
+		return &object.Integer{Value: lVal - rVal}
+	case "*":
+		return &object.Integer{Value: lVal * rVal}
+	case "/":
+		return &object.Integer{Value: lVal / rVal}
+	case "<":
+		return booleanObject(lVal < rVal)
+	case ">":
+		return booleanObject(lVal > rVal)
+	case "==":
+		return booleanObject(lVal == rVal)
+	case "!=":
+		return booleanObject(lVal != rVal)
+	default:
+		return object.NULL
+	}
+}
+
+func evalMinusOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return object.NULL
+	}
+	v := right.(*object.Integer).Value
+	return &object.Integer{Value: -v} // notice the flipping of the value here
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case object.TRUE:
+		return object.FALSE
+	case object.FALSE:
+		return object.TRUE
+	case object.NULL:
+		return object.TRUE
+	default:
+		return object.FALSE
+	}
 }
 
 func booleanObject(b bool) *object.Boolean {
