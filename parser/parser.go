841a0fda9fdf67e8cec34fd1c99eab729fb85a10
Peter Johnston <pemjohns@gmail.com>
2018-11-23 09:21:10 -0700

    Move precedences out of parser file

--- parser/parser.go
+++ parser/parser.go
@@ -8,31 +8,6 @@ import (
 	"strconv"
 )
 
-const (
-	_ int = iota
-	LOWEST
-	EQUALS      // ==
-	LESSGREATER // > or <
-	SUM         // +
-	PRODUCT     // *
-	PREFIX      // -x or !x
-	CALL        // myFn(x)
-	INDEX       // array[index]
-)
-
-var precedences = map[token.TokenType]int{
-	token.EQ:       EQUALS,
-	token.NE:       EQUALS,
-	token.LT:       LESSGREATER,
-	token.GT:       LESSGREATER,
-	token.PLUS:     SUM,
-	token.MINUS:    SUM,
-	token.SLASH:    PRODUCT,
-	token.ASTERISK: PRODUCT,
-	token.LPAREN:   CALL,
-	token.LBRACKET: INDEX,
-}
-
 type Parser struct {
 	l      *lexer.Lexer
 	errors []string
---
+++ parser/precedences.go
@@ -0,0 +1,29 @@
+package parser
+
+import "bellamy/token"
+
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -x or !x
+	CALL        // myFn(x)
+	INDEX       // array[index]
+)
+
+var precedences = map[token.TokenType]int{
+	token.EQ:       EQUALS,
+	token.NE:       EQUALS,
+	token.LT:       LESSGREATER,
+	token.GT:       LESSGREATER,
+	token.PLUS:     SUM,
+	token.MINUS:    SUM,
+	token.SLASH:    PRODUCT,
+	token.ASTERISK: PRODUCT,
+	token.LPAREN:   CALL,
+	token.PERIOD:   CALL,
+	token.LBRACKET: INDEX,
+}
